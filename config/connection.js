c1847d9ac7cb22915c6113d2cc9b04e3e1b4d738
Elaine Cadman <elaine.cadman@gmail.com>
2019-01-07 19:05:25 -0800

    heroku creds

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'uv530txieobwnob6',
+    database: 'k5kzjbpq7uiwrxlj'
+  });
+};
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
